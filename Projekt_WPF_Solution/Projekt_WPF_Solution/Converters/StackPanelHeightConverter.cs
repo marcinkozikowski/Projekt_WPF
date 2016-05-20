59bbd0d9e9a813e6c9ff129b9ea2ce48c892892a
MJ <m.januszewski@hotmail.com>
2016-05-20 16:59:32 +0200

    Poprawiłem namespace convertera

--- Projekt_WPF_Solution/Projekt_WPF_Solution/Converters/StackPanelHeightConverter.cs
+++ Projekt_WPF_Solution/Projekt_WPF_Solution/Converters/StackPanelHeightConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace Projekt_WPF_Solution
+namespace Projekt_WPF_Solution.Converters
 {
     class StackPanelHeightConverter : IValueConverter
     {
