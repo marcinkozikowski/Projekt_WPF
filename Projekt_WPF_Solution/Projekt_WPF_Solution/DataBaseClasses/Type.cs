d9e55b41280d71eb16e39f16eb33023891797e22
MK <marci@DESKTOP-NBJRDD4>
2016-05-31 18:58:01 +0200

    klasa Type do tabeli w bazie danych

---
+++ Projekt_WPF_Solution/Projekt_WPF_Solution/DataBaseClasses/Type.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_WPF_Solution.DataBaseClasses
+{
+    class Type
+    {
+        #region Variables
+        private int id { get; set; }                    //ID PRIMARY KEY
+        private string type { get; set; }           //Numery rejestacyjne
+        private double price { get; set; }     // Spalanie na 100/km
+        #endregion
+    }
+}
